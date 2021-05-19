722f89ffd0f2381e1bea313f3a784f1f4472caea
Edu531 <dudu53188@gmail.com>
2021-05-19 17:08:55 -0300

    Project created

---
+++ src/test/java/com/eduardodev/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardodev.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
